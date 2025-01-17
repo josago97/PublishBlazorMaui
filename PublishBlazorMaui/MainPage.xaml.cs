b3118c40f9e55ae8f82eac63308652c58b917155
josago97 <josago2009@hotmail.es>
2025-01-17 10:59:52 +0100

    Init

---
+++ PublishBlazorMaui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PublishBlazorMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
